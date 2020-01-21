ff28f23fcb66c103882503de0256b3dded7cceed
Zineb21S <z.attaoui@aui.ma>
2020-01-21 09:44:37 +0100

    UIs

--- app/src/main/java/com/example/mobicomplab1/MainActivity.kt
+++ app/src/main/java/com/example/mobicomplab1/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.mobicomplab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var fabOpened = false
+
+        fab.setOnClickListener {
+            if(!fabOpened){
+              fabOpened = true
+              fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+              fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+            } else {
+              fabOpened = false
+              fab_map.animate().translationY(0f)
+              fab_time.animate().translationY(0f)
+            }
+        }
+
+        fab_time.setOnClickListener{
+            toast("Mobile Computing")
+            startActivity(Intent(applicationContext,TimeActivity::class.java))
+
+        }
+        fab_map.setOnClickListener{
+            toast("Mobile Computing")
+            startActivity(Intent(applicationContext,MapActivity::class.java))
+
+        }
+
     }
 }
